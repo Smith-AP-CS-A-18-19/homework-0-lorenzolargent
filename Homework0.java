16e7e87c0393eea187a9ee01c236c8fb3839fee5
mcsmith01 <mcsmith01@alumni.amherst.edu>
2018-08-29 15:00:42 -0400

    Graded 29-Aug-2018  3:00:42 PM

--- Homework0.java
+++ Homework0.java
@@ -1,3 +1,4 @@
+// lorenzolargent
 /*
  * First and Last Names
  */
@@ -9,8 +10,11 @@ public class Homework0 {
 	 * Ensure that you match case and punctuation exactly
 	 */
 	public static void main(String[] args) {
+		// The S in System needs to be capitalized; it is a special
+		//  case of a time when we access members of a class rather
+		//  than an object
 		system.net.println("Hello, World");
-	 
+
 	}
 
 }
